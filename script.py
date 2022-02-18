22374b37e678cdee3278ebdf2fa474b942386bb7
Jason Ouyang <jasonouyang8000@gmail.com>
2022-02-17 16:06:50 -0800

    connection to database and convert json data

--- script.py
+++ script.py
@@ -1,13 +1,21 @@
 from dotenv import load_dotenv
 import os
 import requests
+import json
+
 
 
 
 # Load Key from env
 load_dotenv()
 key = os.getenv('API_KEY')
-url = os.getenv('URL')
+url = os.getenv('API_URL')
+
 
 # Connection to Spotify API Database
-r = requests.post('https://xgentest.myshopify.com/admin/api/2022-01/graphql.json', auth=('user', 'pass'))
+r = requests.get(f"{url}/collections/198483837086/products.json",  headers={"X-Shopify-Access-Token": key})
+data = json.dumps(r.json())
+print(type(data))
+
+with open('catalog.json', 'w') as output:
+    output.write(data)
