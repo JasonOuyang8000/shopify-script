ee884f45d803bffffc02604f14cb218cc7d6ceb7
Jason Ouyang <jasonouyang8000@gmail.com>
2022-02-17 17:48:42 -0800

    invoke function

--- script.py
+++ script.py
@@ -6,16 +6,18 @@ import json
 
 # Load Key from env
 load_dotenv()
-key = os.getenv('API_KEY')
-url = os.getenv('API_URL')
+api_key = os.getenv('API_KEY')
+api_url = os.getenv('API_URL')
 
+def download_catalog(url, collection_id):
+    # Connection to Spotify API 
+    r = requests.get(f"{url}/collections/{collection_id}/products.json",  headers={"X-Shopify-Access-Token": api_key})
 
-# Connection to Spotify API 
-r = requests.get(f"{url}/collections/198483837086/products.json",  headers={"X-Shopify-Access-Token": key})
+    # Dict to JSON for creating the file.
+    data = json.dumps(r.json(), indent=4)
 
-# Dict to JSON for creating the file.
-data = json.dumps(r.json(), indent=4)
+    # Create the file once data has been converted to JSON obj
+    with open('catalog.json', 'w') as output:
+        output.write(data)
 
-# Create the file once data has been converted to JSON obj
-with open('catalog.json', 'w') as output:
-    output.write(data)
+download_catalog(api_url, 198483837086)
